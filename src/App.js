aa12b58504085406c8b16645661d056889a8af95
1303648865@qq.com <1303648865@qq.com>
2019-09-02 20:15:32 +0800

    牛逼的项目

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.myRef = React.createRef()
+  }
+  state={
+    msg:'你好'
+  }
+  render() {
+    let {msg} = this.state
+    return (
+      <div ref={this.myRef}>
+        hello world{msg}
+        <div ref='aaa'>1111111</div>
+      </div>
+    )
+  }
+  componentWillMount(){
+    console.log(this,"componentWillMount")
+    
+  }
+  componentDidMount(){
+    console.log(this,"componentDidMount")
+    this.setState({msg:'zx'})
+    console.log(this.myRef.current.innerText)
+  }
+  componentWillReceiveProps(){
+    console.log(this,"componentWillReceiveProps")
+
+  }
+  shouldComponentUpdate(){
+    console.log(this,"shouldComponentUpdate")
+    return true
+  }
+  componentWillUpdate(){
+    console.log(this,"componentWillUpdate")
+    console.log(this.refs)
+  }
+  componentDidUpdate(){
+    console.log(this,"componentDidUpdate")
+    
+  }
+  componentWillUnmount(){
+    console.log(this,"componentWillUnmount")
+
+  }
+  
 }
 
-export default App;
